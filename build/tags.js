ba389a861c4fa55af2220e3f99f8b287d57915fd
Dan Duvall <dduvall@wikimedia.org>
2015-06-01 16:38:45 -0700

    Highlight step arguments
    Implemented highlighting of step arguments in the name by splitting the
    string into fragments, according to the argument offsets, and rendering
    them in separate spans.
    
    Change-Id: I086a382b8e548fceae683432e11682953da571bc

--- build/tags.js
+++ build/tags.js
@@ -248,12 +248,49 @@ riot.tag('rt-feature', '<p><rt-tag each="{ tags }"></rt-tag></p> <h4 class="list
 	
 });
 
-riot.tag('rt-step', '<p class="list-group-item-text"> <span class="keyword">{ keyword }</span> <span class="step-name">{ name }</span> </p> <p if="{ match.location }" class="list-group-item-text"> <small>{ match.location }</small> </p> <pre if="{ result.error_message }" onclick="{ showErrorMessage }" class="list-group-item-text error-message collapsed">{ result.error_message }</pre>', 'rt-step > .error-message { transition: max-height 0.6s ease; max-height: 500em; } rt-step > .error-message.collapsed { cursor: pointer; max-height: 3em; white-space: nowrap; text-overflow: ellipsis; overflow: hidden; }', function(opts) {
+riot.tag('rt-step', '<p class="list-group-item-text"> <span class="keyword">{ keyword }</span> <span each="{ nameFragments }" class="{ argument: isArgument }">{ value }</span> </p> <p if="{ match.location }" class="list-group-item-text"> <small>{ match.location }</small> </p> <pre if="{ result.error_message }" onclick="{ showErrorMessage }" class="list-group-item-text error-message collapsed">{ result.error_message }</pre>', 'rt-step > .error-message { transition: max-height 0.6s ease; max-height: 500em; } rt-step > .error-message.collapsed { cursor: pointer; max-height: 3em; white-space: nowrap; text-overflow: ellipsis; overflow: hidden; } rt-step .argument { font-weight: bold; }', function(opts) {
 		var self = this;
+
+		
+		function fragmentize(name, match) {
+			var frags = [],
+					pos = 0;
+
+			if (typeof match === 'undefined' || typeof match.arguments === 'undefined') {
+				return [ { isArgument: false, value: name } ];
+			}
+
+			for (var i = 0; i < match.arguments.length; i++) {
+				var arg = match.arguments[i];
+
+				if (typeof arg.offset !== 'undefined') {
+
+					if (pos < arg.offset) {
+						frags.push({ isArgument: false, value: name.substr(pos, arg.offset - pos) });
+					}
+
+					frags.push({ isArgument: true, value: arg.val });
+
+					pos = arg.offset + arg.val.length;
+				}
+			}
+
+			if (pos < name.length) {
+				frags.push({ isArgument: false, value: name.substr(pos) });
+			}
+
+			return frags;
+		}
 
 		self.showErrorMessage = function (e) {
 			$(e.target).removeClass('collapsed');
 		};
+
+		self.on('update', function () {
+			if (self.name) {
+				self.nameFragments = fragmentize(self.name, self.match);
+			}
+		});
 	
 });
 
--- src/rt-step.tag
+++ src/rt-step.tag
@@ -1,6 +1,7 @@
 <rt-step>
 	<p class="list-group-item-text">
-		<span class="keyword">{ keyword }</span> <span class="step-name">{ name }</span>
+		<span class="keyword">{ keyword }</span>
+		<span each={ nameFragments } class={ argument: isArgument }>{ value }</span>
 	</p>
 	<p if={ match.location } class="list-group-item-text">
 		<small>{ match.location }</small>
@@ -23,13 +24,59 @@
 			text-overflow: ellipsis;
 			overflow: hidden;
 		}
+
+		rt-step .argument {
+			font-weight: bold;
+		}
 	</style>
 
 	<script>
 		var self = this;
 
+		/**
+		 * Represents the given step name as an array of text fragments so that we
+		 * can render the arguments differently.
+		 */
+		function fragmentize(name, match) {
+			var frags = [],
+					pos = 0;
+
+			if (typeof match === 'undefined' || typeof match.arguments === 'undefined') {
+				return [ { isArgument: false, value: name } ];
+			}
+
+			for (var i = 0; i < match.arguments.length; i++) {
+				var arg = match.arguments[i];
+
+				if (typeof arg.offset !== 'undefined') {
+					// Leading string before current argument
+					if (pos < arg.offset) {
+						frags.push({ isArgument: false, value: name.substr(pos, arg.offset - pos) });
+					}
+
+					// Argument value
+					frags.push({ isArgument: true, value: arg.val });
+
+					pos = arg.offset + arg.val.length;
+				}
+			}
+
+			// Trailing string after all arguments
+			if (pos < name.length) {
+				frags.push({ isArgument: false, value: name.substr(pos) });
+			}
+
+			return frags;
+		}
+
 		self.showErrorMessage = function (e) {
 			$(e.target).removeClass('collapsed');
 		};
+
+		self.on('update', function () {
+			if (self.name) {
+				self.nameFragments = fragmentize(self.name, self.match);
+			}
+		});
 	</script>
 </rt-step>
